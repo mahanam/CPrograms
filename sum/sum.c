9d93eccd4481425213e105eba00f6c54eddad4fd
Mahadev <mahaalok@gmail.com>
2023-04-11 10:58:50 +0800

    Adding the file sum.c

---
+++ sum/sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    // Declaration of the variables
+    int i4Num1;
+    int i4Num2;
+    int i4SumResult;
+
+    printf("Enter the num1:");
+    scanf("%d", &i4Num1);
+
+    printf("Enter the num2:");
+    scanf("%d", &i4Num2);
+
+    // Main sum logic
+    i4SumResult = i4Num1 + i4Num2;
+
+    // Printing the sum output
+    printf("Result of 2 given numbers is %d\n", i4SumResult);
+
+    // exit from the main
+    return 0;
+}
